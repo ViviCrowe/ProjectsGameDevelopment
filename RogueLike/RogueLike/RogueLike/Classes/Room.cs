1c272a6e0eebe6ac7c5145e270fc6f3c3e835249
Jes <jessica.hauwehde001@stud.fh-dortmund.de>
2022-11-30 16:21:51 +0100

    add viewport and object lists for collision checks

--- RogueLike/RogueLike/RogueLike/Classes/Room.cs
+++ RogueLike/RogueLike/RogueLike/Classes/Room.cs
@@ -1,11 +1,7 @@
 ﻿using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RogueLike.Classes
 {
@@ -13,13 +9,17 @@ namespace RogueLike.Classes
     {
         public Tile[,] Tiles { get; set; }
         public Vector2 Position;
+        public List<GameObject> passiveObjects = new List<GameObject>();
+        public List<Entity> activeObjects = new List<Entity>();
+        private Viewport viewport;
 
-        public Room()
+        public Room(Viewport viewport)
         {
+            this.viewport = viewport;
             //Es funktionieren aktuell nur ungerade Anzahlen
             Tiles = new Tile[11,11];
-            Position.X = (1920f / 2) - ((Tiles.GetLength(0) - 1)*64 /2);
-            Position.Y = (1080f / 2) - ((Tiles.GetLength(0) - 1)*64 / 2);
+            Position.X = (viewport.Width / 2) - ((Tiles.GetLength(0) - 1)*64 /2);
+            Position.Y = (viewport.Height / 2) - ((Tiles.GetLength(0) - 1)*64 / 2);
         }
 
         public void LoadRoomAssets(ContentManager content)
@@ -31,19 +31,18 @@ namespace RogueLike.Classes
                     if(i == 0 || j == 0 || i == Tiles.GetLength(0) -1 || j == Tiles.GetLength(0) - 1)
                     {
                         Tiles[i, j] = new Tile(Position, true);
-                        Tiles[i, j].LoadTileAssets(content, "wall");
+                        Tiles[i, j].LoadAssets(content, "wall");
+                        passiveObjects.Add(Tiles[i, j]);
                     } 
                     else
                     {
                         Tiles[i, j] = new Tile(Position, false);
-                        Tiles[i, j].LoadTileAssets(content, "Grass_normal");
+                        Tiles[i, j].LoadAssets(content, "Grass_normal");
                     }
-                    
-
                     
                     Position.X += 64;
                 }
-                Position.X = 1920f / 2 - (Tiles.GetLength(0) - 1) * 64 / 2;
+                Position.X = viewport.Width / 2 - (Tiles.GetLength(0) - 1) * 64 / 2;
                 Position.Y += 64;
             }
         }
@@ -53,8 +52,8 @@ namespace RogueLike.Classes
             for (int i = 0; i < Tiles.GetLength(0); i++)
             {
                 for (int j = 0; j < Tiles.GetLength(1); j++)
-                {
-                    Tiles[i, j].DrawTile(spriteBatch);
+                {                   
+                    Tiles[i, j].Draw(spriteBatch);
                 }
             }
         }
