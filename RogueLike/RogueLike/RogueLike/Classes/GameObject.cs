89941b05e6954974dd807072e0e19f5dd4f5ffef
Jes <jessica.hauwehde001@stud.fh-dortmund.de>
2022-12-17 21:57:42 +0100

    add layerDepth to Draw, add name to LoadAssets

--- RogueLike/RogueLike/RogueLike/Classes/GameObject.cs
+++ RogueLike/RogueLike/RogueLike/Classes/GameObject.cs
@@ -2,14 +2,30 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 
-public abstract class GameObject {
+public abstract class GameObject
+{
     public Texture2D texture;
+
     public Vector2 position;
-    public BoundingBox boundingBox;    
-                
-    public void Draw(SpriteBatch spriteBatch) {
-        spriteBatch.Draw(texture, position, null, Color.White, 0f, new Vector2(texture.Width/2f, texture.Height/2f),
-         1.0f, SpriteEffects.None, 1.0f);
+
+    public BoundingBox boundingBox;
+
+    public void Draw(SpriteBatch spriteBatch, float layerDepth)
+    {
+        spriteBatch
+            .Draw(texture,
+            position,
+            null,
+            Color.White,
+            0f,
+            new Vector2(texture.Width / 2f, texture.Height / 2f),
+            1.0f,
+            SpriteEffects.None,
+            layerDepth);
     }
-    public abstract void LoadAssets(ContentManager Content);
+
+    public void LoadAssets(ContentManager content, string name)
+    {
+        texture = content.Load<Texture2D>(name);
+    }
 }
