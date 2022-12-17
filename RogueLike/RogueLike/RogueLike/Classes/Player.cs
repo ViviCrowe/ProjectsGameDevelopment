8ba0b4ac8816c196f8f8f96b84b3b87f2d068ccf
Jes <jessica.hauwehde001@stud.fh-dortmund.de>
2022-12-17 21:58:03 +0100

    simplify

--- RogueLike/RogueLike/RogueLike/Classes/Player.cs
+++ RogueLike/RogueLike/RogueLike/Classes/Player.cs
@@ -1,25 +1,12 @@
 ﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 
-namespace RogueLike.Classes
+public class Player : Entity
 {
-    public class Player : Entity
+    public Player(Viewport viewport) :
+        base(viewport)
     {
-        public Player(Viewport viewport) : base(viewport)
-        {
-            position = new Vector2(viewport.Width/2, viewport.Height/2);
-        }
-
-        public override void LoadAssets(ContentManager content)
-        {
-            texture = content.Load<Texture2D>("character");
-        }
-
-        public new void Draw(SpriteBatch spriteBatch)
-        {
-            spriteBatch.Draw(texture, position, null, Color.White, 0,
-                new Vector2(texture.Width / 2, texture.Height / 2), 1f, SpriteEffects.None, 0);
-        }
+        position = new Vector2(viewport.Width / 2, viewport.Height / 2);
+        weapon = new Weapon(); // nur für Testzwecke, zu Beginn des Spiels hat der Spieler keine Waffe (= Fäuste also null)
     }
 }
