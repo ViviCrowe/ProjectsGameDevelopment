34646975f12b94eb2981651fbabce0fc3fda9519
Jes <jessica.hauwehde001@stud.fh-dortmund.de>
2022-12-17 21:58:34 +0100

    simplify

--- RogueLike/RogueLike/RogueLike/Classes/Tile.cs
+++ RogueLike/RogueLike/RogueLike/Classes/Tile.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.Graphics;
 
 namespace RogueLike.Classes
 {
@@ -13,16 +11,6 @@ namespace RogueLike.Classes
         {
             position = tilePosition;
             this.isWall = isWall;
-        }
-
-        public override void LoadAssets(ContentManager content) 
-        {
-            throw new NotSupportedException();
-        }
-
-        public void LoadAssets(ContentManager content, string name) 
-        {
-            texture = content.Load<Texture2D>(name);
         }
     }
 }
