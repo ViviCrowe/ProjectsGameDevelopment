a457cd9e92fe87c9f4ab67241ac35d92e5326c41
Jes <jessica.hauwehde001@stud.fh-dortmund.de>
2022-12-17 21:58:23 +0100

    add items list

--- RogueLike/RogueLike/RogueLike/Classes/Room.cs
+++ RogueLike/RogueLike/RogueLike/Classes/Room.cs
@@ -1,59 +1,93 @@
-﻿using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.Graphics;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Numerics;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace RogueLike.Classes
 {
     public class Room
     {
         public Tile[,] Tiles { get; set; }
+
         public Vector2 Position;
+
         public List<GameObject> passiveObjects = new List<GameObject>();
+
         public List<Entity> activeObjects = new List<Entity>();
+
+        public List<Weapon> items = new List<Weapon>(); // haben (noch?) keine item klasse
+
         private Viewport viewport;
 
         public Room(Viewport viewport)
         {
             this.viewport = viewport;
+
             //Es funktionieren aktuell nur ungerade Anzahlen
-            Tiles = new Tile[11,11];
-            Position.X = (viewport.Width / 2) - ((Tiles.GetLength(0) - 1)*64 /2);
-            Position.Y = (viewport.Height / 2) - ((Tiles.GetLength(0) - 1)*64 / 2);
+            Tiles = new Tile[11, 11];
+            Position.X =
+                (viewport.Width / 2) - ((Tiles.GetLength(0) - 1) * 64 / 2);
+            Position.Y =
+                (viewport.Height / 2) - ((Tiles.GetLength(0) - 1) * 64 / 2);
         }
 
-        public void LoadRoomAssets(ContentManager content)
+        public void LoadAssets(ContentManager content)
         {
             for (int i = 0; i < Tiles.GetLength(0); i++)
             {
                 for (int j = 0; j < Tiles.GetLength(1); j++)
                 {
-                    if(i == 0 || j == 0 || i == Tiles.GetLength(0) -1 || j == Tiles.GetLength(0) - 1)
+                    if (
+                        i == 0 ||
+                        j == 0 ||
+                        i == Tiles.GetLength(0) - 1 ||
+                        j == Tiles.GetLength(0) - 1
+                    )
                     {
                         Tiles[i, j] = new Tile(Position, true);
                         Tiles[i, j].LoadAssets(content, "wall");
                         passiveObjects.Add(Tiles[i, j]);
-                    } 
+                    }
                     else
                     {
                         Tiles[i, j] = new Tile(Position, false);
                         Tiles[i, j].LoadAssets(content, "Grass_normal");
                     }
-                    
+
                     Position.X += 64;
                 }
-                Position.X = viewport.Width / 2 - (Tiles.GetLength(0) - 1) * 64 / 2;
+                Position.X =
+                    viewport.Width / 2 - (Tiles.GetLength(0) - 1) * 64 / 2;
                 Position.Y += 64;
             }
         }
 
-        public void DrawRoom(SpriteBatch spriteBatch)
+        public void LoadItemAssets(ContentManager content)
+        {
+            foreach (Weapon item in items)
+            {
+                if (item != null)
+                {
+                    item.LoadAssets(content, "sword");
+                }
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
         {
             for (int i = 0; i < Tiles.GetLength(0); i++)
             {
                 for (int j = 0; j < Tiles.GetLength(1); j++)
-                {                   
-                    Tiles[i, j].Draw(spriteBatch);
+                {
+                    Tiles[i, j].Draw(spriteBatch, 1f);
+                }
+            }
+
+            foreach (Weapon item in items)
+            {
+                if (item != null)
+                {
+                    item.Draw(spriteBatch, 0.9f);
                 }
             }
         }
