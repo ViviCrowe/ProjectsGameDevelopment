0b5f69edae61cf4074ce93fd78a3451d0d76652b
RanHana <56399825+RanHana@users.noreply.github.com>
2022-12-19 12:16:56 +0100

    Added Folder Abilities Added Enum ConcreteAktivAbility Added Classes AktivAbility and PassivAbility

---
+++ RogueLike/RogueLike/RogueLike/Classes/Abilities/AktivAbility.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using SharpDX.Direct2D1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RogueLike.Classes.Abilities
+{
+    public class AktivAbility
+    {
+        private float castDuration;
+        private int abilityDamage;
+        private ConcreteAktiveAbility abilityType;
+        private int abilityHealthPoints;
+
+        private Texture2D abilityTexture;
+        private SoundEffect abilitySoundEffect;
+
+        public AktivAbility(Texture2D abilityTexture, SoundEffect abilitySoundEffects, ConcreteAktiveAbility abilityType)
+        {
+            this.abilityTexture = abilityTexture;
+            this.abilitySoundEffect = abilitySoundEffects;
+            this.abilityType = abilityType;
+        }
+
+        public void CastAbility()
+        {
+            if (abilityType == ConcreteAktiveAbility.FIREBALL)
+            {
+                Fireball();
+            }
+            else if (abilityType == ConcreteAktiveAbility.EARTHBALL)
+            {
+                Earthball();
+            }
+            else if (abilityType == ConcreteAktiveAbility.AIRBALL)
+            {
+                Airball();
+            }
+        }
+
+        private void Fireball()
+        {
+            //TODO
+        }
+
+        private void Earthball()
+        {
+            //TODO
+        }
+
+        private void Airball()
+        {
+            //TODO
+        }
+    }
+}
---
+++ RogueLike/RogueLike/RogueLike/Classes/Abilities/PassivAbillity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RogueLike.Classes.Abilities
+{
+    public class PassivAbillity
+    {
+        private float attackSpeed;
+        private int healthPoits;
+        private int attackDamage;
+        private float movementSpeed;
+
+        private Player player;
+        public PassivAbillity(Player player)
+        {
+            this.player = player;
+        }
+
+        public void AddToPlayerStats()
+        {
+            //TODO
+        }
+
+        public void RemoveFromPlayerStats()
+        {
+            //TODO
+        }
+    }
+}
