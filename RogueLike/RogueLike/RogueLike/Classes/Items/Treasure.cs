a3da4bd99c6049f79c8556c6d9093cfe41123a6e
Jes <jessica.hauwehde001@stud.fh-dortmund.de>
2023-01-16 00:42:08 +0100

    add class

---
+++ RogueLike/RogueLike/RogueLike/Classes/Items/Treasure.cs
@@ -0,0 +1,12 @@
+using Microsoft.Xna.Framework;
+
+namespace RogueLike.Classes.Items
+{
+    public class Treasure : GameObject
+    {
+        public Treasure(Vector2 position)
+        {
+            this.Position = position;
+        }
+    }
+}
