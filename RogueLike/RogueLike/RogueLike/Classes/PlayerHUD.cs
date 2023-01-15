f929cd340a2ef9726dd3064503df1744e41353f3
Jes <jessica.hauwehde001@stud.fh-dortmund.de>
2023-01-15 14:22:32 +0100

    add level, shorten method

--- RogueLike/RogueLike/RogueLike/Classes/PlayerHUD.cs
+++ RogueLike/RogueLike/RogueLike/Classes/PlayerHUD.cs
@@ -3,6 +3,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using RogueLike.Classes.Abilities;
+using RogueLike.Classes.Items;
 using RogueLike.Classes.Weapons;
 
 namespace RogueLike.Classes
@@ -18,12 +19,14 @@ namespace RogueLike.Classes
         private String teethValue;
 
         private String healthValue;
+        private String levelValue;
 
         private SpriteFont font;
 
         private Texture2D weaponTexture;
 
         private Texture2D abilityTexture;
+        private Texture2D teethTexture;
 
         public PlayerHUD(Player player)
         {
@@ -42,16 +45,19 @@ namespace RogueLike.Classes
             {
                 abilityTexture = aktivAbility.abilityTexture;
             }
+            teethTexture = Wallet.Texture;
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
+            spriteBatch.Draw(teethTexture, new Vector2(60, 12.5f), Color.White);
             spriteBatch.DrawString(font, teethValue, new Vector2(100, 12.5f), Color.White);
-            spriteBatch.DrawString(font, healthValue, new Vector2(200, 12.5f), Color.White);
+            spriteBatch.DrawString(font, "Health    " + healthValue, new Vector2(200, 12.5f), Color.White);
+            spriteBatch.DrawString(font, "Level    " + levelValue, new Vector2(600, 12.5f), Color.White);
             if (weaponTexture != null)
-                spriteBatch.Draw(weaponTexture, new Vector2(400, 0), Color.White);
+                spriteBatch.Draw(weaponTexture, new Vector2(700, 0), Color.White);
             if (abilityTexture != null)
-                spriteBatch.Draw(abilityTexture, new Vector2(600, 0), Color.White);
+                spriteBatch.Draw(abilityTexture, new Vector2(900, 0), Color.White);
         }
 
         public void Update(Player player)
@@ -60,6 +66,7 @@ namespace RogueLike.Classes
             this.healthValue = player.CurrentHealth + "/" + player.MaximumHealth;
             this.weaponSlot = player.EquippedWeapon;
             this.aktivAbility = player.aktivAbility;
+            this.levelValue = player.Level.ToString();
         }
     }
 }
