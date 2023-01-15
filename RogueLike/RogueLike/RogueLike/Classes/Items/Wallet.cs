a404828217714e8e98cd8a222b37fcacef6a62c7
Jes <jessica.hauwehde001@stud.fh-dortmund.de>
2023-01-15 14:22:12 +0100

    make texture static

--- RogueLike/RogueLike/RogueLike/Classes/Items/Wallet.cs
+++ RogueLike/RogueLike/RogueLike/Classes/Items/Wallet.cs
@@ -1,8 +1,11 @@
+using Microsoft.Xna.Framework.Graphics;
+
 namespace RogueLike.Classes.Items
 {
     public class Wallet : GameObject
     {
         public int Value { get; set; }
+        public new static Texture2D Texture { get; set; }
 
         public Wallet(int value)
         {
